e5ee21700b41f27e3a69bd9c4d81ff69d57c7f09
ryan-qiyu-jiang <ryan.qiyu.jiang@gmail.com>
2017-11-11 13:38:10 -0500

    js try 2

--- assets/js/email_send.js
+++ assets/js/email_send.js
@@ -1,3 +1,4 @@
+/*
 (function() {
   $(document).ready(function() {
     return $('#contact-form').submit(function(e) {
@@ -30,3 +31,25 @@
   });
 
 }).call(this);
+*/
+var $contactForm = $('#contact-form');
+$contactForm.submit(function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: '//formspree.io/ryan.qiyu.jiang@gmail.com',
+    method: 'POST',
+    data: $(this).serialize(),
+    dataType: 'json',
+    beforeSend: function() {
+      $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+    },
+    success: function(data) {
+      $contactForm.find('.alert--loading').hide();
+      $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+    },
+    error: function(err) {
+      $contactForm.find('.alert--loading').hide();
+      $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+    }
+  });
+});
